f8ebcb41853dba0131b2580db5a87a9dde9e15f2
Julien BAYONNE CHENARD <julien.bayonne-chenard@outlook.com>
2025-10-07 14:30:08 +0200

    move even better ; git push

--- src/graphic_lib/handle_input.c
+++ src/graphic_lib/handle_input.c
@@ -17,13 +17,13 @@ int	handle_key_input_bis(int keysym, t_mini *mini, t_cam *cam)
 	if (keysym == 0xff52)
 	{
 		cam->pos = vec_add(cam->pos, vec_scale(cam->vec_dir, 3));
-		set_up_cam(&mini);
+		set_up_cam(mini);
 		run_thread(mini);
 	}
 	if (keysym == 0xff54)
 	{
 		cam->pos = vec_substact(cam->pos, vec_scale(cam->vec_dir, 3));
-		set_up_cam(&mini);
+		set_up_cam(mini);
 		run_thread(mini);
 	}
 	mlx_put_image_to_window(mini->display.mlx, mini->display.mlx_win,
@@ -42,13 +42,13 @@ int	handle_key_input(int keysym, t_mini *mini)
 	if (keysym == 0xff53)
 	{
 		cam->pos = vec_add(cam->pos, vec_scale(cam->right, 3));
-		set_up_cam(&mini);
+		set_up_cam(mini);
 		run_thread(mini);
 	}
 	if (keysym == 0xff51)
 	{
 		cam->pos = vec_substact(cam->pos, vec_scale(cam->right, 3));
-		set_up_cam(&mini);
+		set_up_cam(mini);
 		run_thread(mini);
 	}
 	return (handle_key_input_bis(keysym, mini, cam));
