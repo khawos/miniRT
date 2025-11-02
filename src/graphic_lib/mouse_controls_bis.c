bdf8ca8a27db7ecdc7b9af57dd2213d9043e3911
Adam Medeneche <adammdn92@gmail.com>
2025-11-02 16:30:58 +0100

    fixed cam rotation

--- src/graphic_lib/mouse_controls_bis.c
+++ src/graphic_lib/mouse_controls_bis.c
@@ -6,19 +6,28 @@
 /*   By: amedenec <amedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 15:27:47 by jbayonne          #+#    #+#             */
-/*   Updated: 2025/11/02 15:35:36 by amedenec         ###   ########.fr       */
+/*   Updated: 2025/11/02 16:30:18 by amedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-t_boolean	is_a_bad_rotation(t_vec3 vec_dir1, t_vec3 vec_dir2)
+t_boolean	is_a_bad_rotation(t_vec3 v_new, t_vec3 v_old)
 {
-	if ((vec_dir1.x > 0 && vec_dir2.x < 0)
-		&& (vec_dir1.y < 0 && vec_dir2.y > 0))
+	if ((v_old.x > 0 && v_new.x < 0)
+		&& (v_old.y < 0 && v_new.y > 0) && (v_old.z > 0.999 || v_old.z < 0.001))
 		return (true);
-	if ((vec_dir1.x < 0 && vec_dir2.x > 0)
-		&& (vec_dir1.y > 0 && vec_dir2.y < 0))
+	if ((v_old.x < 0 && v_new.x > 0)
+		&& (v_old.y > 0 && v_new.y < 0) && (v_old.z > 0.999 || v_old.z < 0.001))
+		return (true);
+	if ((v_old.x > 0 && v_new.x > 0)
+		&& (v_old.x < 0 && v_new.y < 0) && (v_old.z > 0.999 || v_old.z < 0.001))
+		return (true);
+	if ((v_old.x > 0 && v_old.y > 0)
+		&& (v_new.x < 0 && v_new.y < 0) && (v_old.z > 0.999 || v_old.z < 0.001))
+		return (true);
+	if ((v_old.x < 0 && v_old.y < 0)
+		&& (v_new.x > 0 && v_new.y > 0) && (v_old.z > 0.999 || v_old.z < 0.001))
 		return (true);
 	return (false);
 }
@@ -35,28 +44,20 @@ void	handle_mouse_compute(t_cam *cam, t_mini *mini, double dx, double dy)
 		up_world = (t_vec3){0, 0, 1};
 		yaw = dx * -0.002;
 		pitch = dy * -0.002;
-		vec_dir_tmp = vec_rotate(cam->vec_dir, up_world, -yaw);
-		// if (is_a_bad_rotation(cam->vec_dir, vec_dir_tmp))
-		// {
-		// //	printf("HAHAHA\n");
-		// 	return ;
-		// }
-		// else
-		// 	cam->vec_dir = vec_dir_tmp;
-		//printf("x:%f y:%f z:%f\n", cam->vec_dir.x, cam->vec_dir.y, cam->vec_dir.z);
-	
+		vec_dir_tmp = cam->vec_dir;
+		cam->vec_dir = vec_rotate(cam->vec_dir, up_world, -yaw);
 		get_right_local_vector(mini, mini->cam_lock);
 		get_up_local_vector(mini, mini->cam_lock);
 		cam->vec_dir = vec_rotate(cam->vec_dir, cam->right, -pitch);
 		cam->vec_dir = vec_normalize(cam->vec_dir);
 		get_right_local_vector(mini, mini->cam_lock);
 		get_up_local_vector(mini, mini->cam_lock);
+		if (is_a_bad_rotation(cam->vec_dir, vec_dir_tmp))
+			cam->vec_dir = vec_dir_tmp;
 	}
 	else
-	{
-		cam->pos = vec_add(cam->pos, vec_scale(cam->right, dx * 0.3));
-		cam->pos = vec_add(cam->pos, vec_scale(cam->up, -dy * 0.3));
-	}
+		cam->pos = vec_add(vec_add(cam->pos, vec_scale(cam->right, dx * 0.3)),
+				vec_scale(cam->up, -dy * 0.3));
 }
 
 int	handle_mouse_move(int x, int y, t_mini *mini)
